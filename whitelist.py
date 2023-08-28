27995f365ddd83c678abf1deab1f924d28ceffd1
DongJu93 <spdlqj011@gmail.com>
2023-08-28 15:34:49 +0900

    add whitelist.py

---
+++ whitelist.py
@@ -0,0 +1,29 @@
+whitelist_urls = [
+    "trendmicro.com",
+    "attack.mitre.org",
+    "www.ithome.com.tw",
+    "hitcon.org",
+    "github.com",
+    "securelist.com",
+    "bit.ly",
+    "www.symantec.com",
+    "researchcenter.paloaltonetworks.com",
+    "twitter.com",
+    "www.clearskysec.com",
+    "www.fireeye.com",
+    "unit42.paloaltonetworks.com",
+    "www.welivesecurity.com",
+    "en.wikipedia.org",
+    "www.arbornetworks.com",
+    "www.virustotal.com",
+    "mcafee.com"
+]
+
+whitelist_file_names = [
+    ".com",
+    ".org",
+    ".www",
+    ".jp",
+    ".net",
+    ".cn"
+]
